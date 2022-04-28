874d7849c6031bc948260838a762b82a61da4889
nomedal <m.nomedal@protonmail.com>
2022-04-28 22:23:04 +0200

    remove heroku compatibility and update README

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # discord-pushup-bot
-A simple bot with a simple purpose. To post how many pushups one should do every morning at 05:00. 
+A simple bot with a simple purpose. To post how many pushups one should do every morning at 06:00. 
 
 It randomizes between 120 and 170 pushups, but can be set to an arbitrary range, or fixed number.
 
-This repo has extra files for Heroku auto deployment and hosting compatibility, but the program itself is not dependent on these files. Meaning you can host it yourself without using Heroku.
+It is fully self hosted using python 3.8.10.
 
 # Dependencies
 ```
 discord, random, asyncio, datetime, os
 ```
 # Usage
-`"DISCORD_BOT_TOKEN"` is your personal bot token, and needs to be added either as an environment variable, server variable(for server use) or as clear text. 
+The variables below needs to be added either as an environment variable, server variable(for server use) or as clear text.
+`"DISCORD_BOT_TOKEN"` is your personal bot token.
+`CHANNEL_ID` is your channel ID.
+`CUSTOM ACTIVITY STATUS` is an optional custom activity status for your bot. Can be commented out or removed if not wanted.
+
+It's highly recommended using `screen` when running the bot.
 
 To run from terminal:
 ```
 python main.py
 ```
-
-To run for server, see server specific setup guide. 
--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-import discord
+import discord,asyncio,os
 from discord.ext import commands, tasks
-import random
-import asyncio
 from datetime import datetime
-import os
+import random
 
-class PushupBot(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        # create the background task and run it in the background
-        self.bg_task = self.loop.create_task(self.my_background_task())
+token = "DISCORD_BOT_TOKEN" # replace DISCORD_BOT_TOKEN with server token
+bot = commands.Bot(command_prefix='.')
+message_channel_id = CHANNEL_ID # replace CHANNEL_ID with numerical channel ID
+send_time='06:00' # send time on 24 hour format
 
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+@bot.event
+async  def on_ready():
+    change_status.start()
+    print('bot is active')
 
-    async def my_background_task(self):
-        await self.wait_until_ready()
-        channel = self.get_channel(message_channel_id) # channel ID goes here
-        while not self.is_closed():
-            now=datetime.strftime(datetime.now(),'%H:%M')
-            value = random.randint(120, 170)
-            if now == send_time:
-                await channel.send("Today we will do %d pushups." % value)
-                await asyncio.sleep(60) # task runs every 60 seconds
+@tasks.loop(seconds=2)
+async def change_status():
+    channel = bot.get_channel(message_channel_id)
+    await bot.change_presence(activity=discord.Game('CUSTOM ACTIVITY STATUS')) # optional custom activity status, can be commented out
+    now=datetime.strftime(datetime.now(),'%H:%M')
+    value = random.randint(120, 170) # number of pushups value range
+    if now == send_time:
+        await channel.send("Today we will do %d pushups." % value)
+        await asyncio.sleep(61) # task runs every 61 seconds
 
-if __name__ == "__main__":
-    send_time='05:00'
-    token = os.getenv("DISCORD_BOT_TOKEN")
-    message_channel_id = 792133366295822337
-
-    client = PushupBot()
-    client.run(token)
+bot.run(token)
